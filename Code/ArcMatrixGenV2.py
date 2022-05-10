619b80d2012a5fe4ec0917ba2b3e385e97d3d0d2
Sichrono <yuanlongd@gmail.com>
2022-05-10 02:41:20 -0400

    Added more classes for different shapes

--- Code/ArcMatrixGenV2.py
+++ Code/ArcMatrixGenV2.py
@@ -2,11 +2,16 @@ from math import cos, sin, floor, radians, pi
 import lxml.etree as etree
 import random
 import svgwrite
+from wand.api import library
+import wand.color
+import wand.image   
+
 class PriorityQueue(): #https://youtu.be/wptevk0bshY
     pass
 
 class Queue:
-    def __init__(self):
+    def __init__(self, id):
+        self.id = id
         self._L = []
 
     def enqueue(self, item):
@@ -26,33 +31,68 @@ class Queue:
 
     def isempty(self):
         return len(self) == 0
+class ArcSVGShape():
+    pass
 
-class ArcSVGCircle():
-    def __init__(self, idnum):
-        self.id = f"circ{idnum}"
-class ArcSVGPolyGon():
-    def __init__(self, idnum):
-        self.id = f"polyG{idnum}"
-class ArcSVGPolyLine():
-    def __init__(self, idnum):
-        self.id = f"polyL{idnum}"
-class ArcSVGShapeEntry():
-    def __init__(self, id, layer):
-        pass
+class ArcSVGCircle(ArcSVGShape):
+    def __init__(self, id, center, majorRadius, strokeColor = "purple", strokeWidth = 2, Opaque = False):
+        self.id = f"Circle{id}"
+        self.center = center
 
-class ArcSVGMain():#basically keep track of layers 
+class ArcSVGRotoryDial(ArcSVGShape):
+    def __init__(self, id, center, majorRadius, minorRadius, strokeColor = "purple", strokeWidth = 2, Opaque = False):
+        self.id = f"RotaryDial{id}"
+        self.center = center
+
+class ArcSVGLineBurst(ArcSVGShape):
+    def __init__(self, id, center, majorRadius, points, strokeColor = "purple", strokeWidth = 2):
+        self.id = f"LineBurst{id}"
+        self.center = center
+
+class ArcSVGStargram(ArcSVGShape):
+    def __init__(self, id, center, majorRadius, points, strokeColor = "purple", strokeWidth = 2):
+        self.id = f"Stargram{id}"
+        self.center = center
+
+class ArcSVGPolyStar(ArcSVGShape):
+    def __init__(self, id, center, majorRadius, minorRadius, points, strokeColor = "purple", strokeWidth = 2, Opaque = False):
+        self.id = f"PolyStar{id}"
+        self.center = center
+
+class ArcSVGPolygon(ArcSVGShape):
+    def __init__(self, id, center, majorRadius, points, strokeColor = "purple", strokeWidth = 2, Opaque = False):
+        self.id = f"Polygon{id}"
+        self.center = center
+
+class ArcSVGMain(ArcSVGShape):#basically keep track of layers 
     def __init__(self):
         self.Layers = Queue() # a layer is made of a queue of entries, FIFO ordering for build execution
-        self.currentLayer = None
-    
+        self.currentLayer = 0
+        
     def newLayer(self):
         self.Layers.enqueue(Queue()) #add a new queue as a layer
     
     def swapLayers(self, layer1, layer2):
-        self.Layers.swapEntries(i1, i2)
+        self.Layers.swapEntries(layer1, layer2)
         
+    def addEntry(self, Etype, center=(0,0)):
+        match Etype:
+            case "Circle":
+                print("circle")
+            case "Polygon":
+                pass
+            case "Stargram":
+                pass
+            case "RotoryDial":
+                pass
+            case "PolyStar":
+                pass
+            case "LineBurst":
+                pass
 
-class ArcSVGProcess():#take stuff from main and pump it out into a proper svg file with svgwrite
+
+
+class ArcSVGProcess(ArcSVGMain):#take stuff from main and pump it out into a proper svg file with svgwrite
     def __init__(self, filename, dimensions, fillrule):
         self.filename = str(filename)
         self.svgDoc = svgwrite.Drawing(filename=str(self.filename) + ".svg",
@@ -60,6 +100,7 @@ class ArcSVGProcess():#take stuff from main and pump it out into a proper svg fi
                                         profile='full',
                                         debug=True)
         self.center = self.getCenter(dimensions)
+
     def parseLayerData(self, layerList):
         pass
 
@@ -90,5 +131,6 @@ class ArcSVGProcess():#take stuff from main and pump it out into a proper svg fi
 
 
 if __name__ == "__main__":
-    g = ArcSVGMain()
+    g = ArcSVGProcess("ArcMatrixV2", (1024,1024), "evenodd")
+    g.addEntry(Etype="Circle")
     
